cf4ada606ea4acdd34c6f11511dd4681786f49a7
Sercan S <83725191+ragiii@users.noreply.github.com>
2022-06-07 16:24:18 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayi giriniz :");
+
+        double n =input.nextDouble();
+        double result=0;
+
+        while(n>0){
+            result += (1/n);
+            n--;
+        }
+        System.out.println("sonuç : "+result);
+    }
+}
